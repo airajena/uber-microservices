9a23eaa9e3b43d9d2a880be3d3ec8314199bbb4c
Aira Jena <airajena.hack@gmail.com>
2025-01-22 10:08:32 +0530

    feat: Add auth service

---
+++ auth-service/src/main/java/com/uber/auth/services/AuthService.java
@@ -0,0 +1,27 @@
+package com.uber.auth.services;
+
+import com.uber.auth.dto.PassengerDto;
+import com.uber.auth.dto.PassengerSignupRequestDto;
+import com.uber.auth.models.Passenger;
+import com.uber.auth.repositories.PassengerRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+@Service
+public class AuthService {
+    private final PassengerRepository passengerRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    public AuthService(PassengerRepository passengerRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.passengerRepository = passengerRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+    public PassengerDto signupPassenger(PassengerSignupRequestDto passengerSignupRequestDto) {
+        Passenger passenger = Passenger.builder()
+                .email(passengerSignupRequestDto.getEmail())
+                .name(passengerSignupRequestDto.getName())
+                .password(bCryptPasswordEncoder.encode(passengerSignupRequestDto.getPassword())) // TODO: Encrypt the password
+                .phoneNumber(passengerSignupRequestDto.getPhoneNumber())
+                .build();
+        Passenger newPassenger = passengerRepository.save(passenger);
+        return PassengerDto.from(newPassenger);
+    }
+}
