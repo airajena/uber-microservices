e1d148c486eb518731a061cfcf6b6339f4990ba8
Aira Jena <airajena.hack@gmail.com>
2025-01-22 10:09:51 +0530

    feat: Update auth controller

--- auth-service/src/main/java/com/uber/auth/controllers/AuthController.java
+++ auth-service/src/main/java/com/uber/auth/controllers/AuthController.java
@@ -2,16 +2,31 @@ package com.uber.auth.controllers;
 
 import com.uber.auth.dto.PassengerDto;
 import com.uber.auth.dto.PassengerSignupRequestDto;
+import com.uber.auth.services.AuthService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 @RestController
 @RequestMapping("/api/v1/auth")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/signup/passenger")
     public ResponseEntity<PassengerDto> signUp(@RequestBody PassengerSignupRequestDto passengerSignupRequestDto) {
-        return null;
+        PassengerDto response = authService.signupPassenger(passengerSignupRequestDto);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    @GetMapping("/signin/passenger")
+    public ResponseEntity<?> signIn() {
+
+        return new ResponseEntity<>(10, HttpStatus.CREATED);
+    }
+
 }
