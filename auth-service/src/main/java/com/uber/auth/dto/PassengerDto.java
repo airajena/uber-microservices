0585add6031ad1d5be2ccb641b85bc9ea9ad3cf2
Aira Jena <airajena.hack@gmail.com>
2025-01-22 09:31:08 +0530

    feat: Add passenger dto

---
+++ auth-service/src/main/java/com/uber/auth/dto/PassengerDto.java
@@ -0,0 +1,17 @@
+package com.uber.auth.dto;
+
+import lombok.*;
+import java.util.Date;
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class PassengerDto {
+    private String id;
+    private String name;
+    private String email;
+    private String password;
+    private String phoneNumber;
+    private Date createdAt;
+}
