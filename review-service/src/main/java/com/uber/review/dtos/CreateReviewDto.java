64b76815e9c3af7f34abfa1494df796aaf4ca9a2
Aira Jena <airajena.hack@gmail.com>
2025-01-24 10:54:27 +0530

    feat: Add create review dto

---
+++ review-service/src/main/java/com/uber/review/dtos/CreateReviewDto.java
@@ -0,0 +1,15 @@
+package com.uber.review.dtos;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateReviewDto {
+
+    private String content;
+
+    private Double rating;
+
+    private Long bookingId;
+}
