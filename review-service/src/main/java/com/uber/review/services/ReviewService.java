3e2a7b47b3190e25d7cc357828d0d32a9ff6a159
Aira Jena <airajena.hack@gmail.com>
2025-01-24 10:32:31 +0530

    feat: Add service layer for review service

--- review-service/src/main/java/com/uber/review/services/ReviewService.java
+++ review-service/src/main/java/com/uber/review/services/ReviewService.java
@@ -2,38 +2,21 @@ package com.uber.review.services;
 
 import com.uber.review.models.Driver;
 import com.uber.review.models.Review;
-import com.uber.review.repositories.BookingRepository;
-import com.uber.review.repositories.DriverRepository;
-import com.uber.review.repositories.ReviewRepository;
-import jakarta.transaction.Transactional;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-
 @Service
-public class ReviewService implements CommandLineRunner {
+public interface ReviewService {
 
-    private ReviewRepository reviewRepository;
-    private final BookingRepository bookingRepository;
-    private final DriverRepository driverRepository;
+    public Optional<Review> findReviewById(Long id);
 
-    public ReviewService(ReviewRepository reviewRepository,
-                         BookingRepository bookingRepository,
-                         DriverRepository driverRepository) {
-        this.reviewRepository = reviewRepository;
-        this.bookingRepository = bookingRepository;
-        this.driverRepository = driverRepository;
-    }
+    public List<Review> findAllReviews();
 
-    @Override
-    @Transactional
-    public void run(String... args) throws Exception {
-        System.out.println("*************");
+    public boolean deleteReviewById(Long id);
 
-        Optional<Driver> d = driverRepository.hqlFindByIdAndLicense(1L, "DL121212");
-        System.out.println(d.get().getName());
-    }
+    public Review publishReview(Review review);
+
+    public Review updateReview(Long id, Review review);
+
 }
